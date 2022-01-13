6e360af7b067cf95fa38cbfec2a0a0b0e945fd68
giiki <guibollellalongo@gmail.com>
2022-01-13 02:05:16 -0300

    gyro fofo

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -38,6 +38,7 @@ const row = new MessageActionRow()
 const row2 = new MessageActionRow()
         .addComponents([button2])
   
+        console.log(user.createdAt)
   let embed = new Discord.MessageEmbed()
   .setDescription(`
   \\🔖**Nome:** ${user.username}
